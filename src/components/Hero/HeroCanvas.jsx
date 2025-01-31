f633a2bbc94b07892b83dd8860d3048a95ad6c99
Ajaya-Tamang <93853672+Ajaya-Tamang@users.noreply.github.com>
2025-02-01 03:05:41 +0545

    Update HeroCanvas.jsx

--- src/components/Hero/HeroCanvas.jsx
+++ src/components/Hero/HeroCanvas.jsx
@@ -1,1 +1,31 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Stars } from '@react-three/drei';
 
+const HeroCanvas = () => {
+  const starsRef = useRef();
+
+  useFrame(() => {
+    starsRef.current.rotation.y += 0.0004;
+    starsRef.current.rotation.x += 0.0002;
+  });
+
+  return (
+    <>
+      <color attach="background" args={['#0F172A']} />
+      <Stars
+        ref={starsRef}
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
+      <ambientLight intensity={0.5} />
+    </>
+  );
+};
+
+export default HeroCanvas;
