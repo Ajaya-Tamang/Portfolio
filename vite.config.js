de4e034dd7b6305ed74dce1881c74365ebbafb19
Ajaya-Tamang <93853672+Ajaya-Tamang@users.noreply.github.com>
2025-02-01 03:22:56 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser'
-  }
+  base: '/portfolio/', // Add this line with your repository name
+})
